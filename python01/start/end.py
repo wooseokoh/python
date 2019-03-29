f3514a400b3924eb4401e65e672e07d0a81fd07e
wooseokoh <48680896+wooseokoh@users.noreply.github.com>
2019-03-29 21:42:53 +0900

    Add files via upload

---
+++ python01/start/end.py
@@ -0,0 +1,10 @@
+#de = input('소속 :')
+#cl = input('과목 :')
+
+lastgrade = input('지난번 성적 :')
+todaygrade = input('이번 성적 :')
+
+
+#print(de)
+#print(cl)
+
---
+++ python01/start/hello.py
@@ -0,0 +1,18 @@
+# basic output : monitor
+
+print('hi');
+
+# basic input : keyboard
+
+name = input('your name :' )
+year = input('your brith year :')
+
+print('your input name is :', name);
+
+print('your input year is :', year);
+
+yearInt = int(year)
+
+age = 2019 - yearInt + 1
+
+print("your age is ", age);
