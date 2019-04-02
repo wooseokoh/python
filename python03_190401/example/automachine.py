78d331e339b6abece984d477ceb5671bafbec524
wooseokoh <48680896+wooseokoh@users.noreply.github.com>
2019-04-02 15:39:04 +0900

    Update automachine.py

--- python03_190401/example/automachine.py
+++ python03_190401/example/automachine.py
@@ -15,17 +15,20 @@ while True:
 
     if set == 0:
         print("프로그램을 종료합니다.")
+        print("다음에 또 이용해 주세요. 잔돈은 %d입니다." % money)
         break
     
     elif set == 1:
         if stock[0] == 0:
             print("Sold out, 다른메뉴를 골라주세요")
+            print("------------------------------")
         else:
             stock[0] -= 1
             cola = 1000
             money = money - cola
             print("코카콜라는 %d입니다. 잔돈은 %d입니다." % (cola, money))
             print("남은재고는 %d개입니다."% stock[0])
+            print("------------------------------")
             
     elif set == 2:
         if stock[1] == 0:
@@ -33,50 +36,62 @@ while True:
         else:
             if money < 1500:
                 print("돈이 모자라네요")
+                print("------------------------------")
             else:
                 stock[1] -= 1
                 poka = 1500
                 money = money - poka
                 print("포카리는 %d입니다. 잔돈은 %d입니다." % (poka, money))
                 print("남은재고는 %d개입니다."% stock[1])
+                print("------------------------------")
             
     elif set == 3:
         if stock[2] == 0:
             print("Sold out, 다른메뉴를 골라주세요")
+            print("------------------------------")
         else:
             if money < 1300:
                 print("돈이 모자라네요")
+                print("------------------------------")
             else:
                 stock[2] -= 1
                 sprite = 1300
                 money = money - sprite
                 print("스프라이트는 %d입니다. 잔돈은 %d입니다." % (sprite, money))
                 print("남은재고는 %d개입니다."% stock[2])
+                print("------------------------------")
             
     elif set == 4:
         if stock[3] == 0:
             print("Sold out, 다른메뉴를 골라주세요")
+            print("------------------------------")
         else:
             if money < 1200:
                 print("돈이 모자라네요")
+                print("------------------------------")
             else:
                 stock[3] -= 1
                 rice = 1200
                 money = money - rice 
                 print("식혜는 %d입니다. 잔돈은 %d입니다." % (rice, money))
                 print("남은재고는 %d개입니다."% stock[3])
+                print("------------------------------")
         
     elif set == 5:
         if stock[4] == 0:
             print("Sold out, 다른메뉴를 골라주세요")
+            print("------------------------------")
         else:
             if money < 1100:
                 print("돈이 모자라네요")
+                print("------------------------------")
             else:
                 stock[4] -= 1
                 orange = 1100
                 money = money - orange
                 print("쌕쌕는 %d입니다. 잔돈은 %d입니다." % (orange, money))
                 print("남은재고는 %d개입니다."% stock[4])
+                print("------------------------------")
     else:
         print("다시입력해주세요.")
+        print("------------------------------")
