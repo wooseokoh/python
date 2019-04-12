e19e33d56131ea04a1aad577442ea1186ee9ca9c
wooseokoh <48680896+wooseokoh@users.noreply.github.com>
2019-04-12 21:23:19 +0900

    Update mainUi.py

--- python12/movieProgram/mainUi.py
+++ python12/movieProgram/mainUi.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from movieProgram.movieUi import insert_infor, update_infor, delete_infor
+from movieProgram.movieUi import insert_infor, update_infor, delete_infor, select_infor, select_all_infor
 
 
 
@@ -10,13 +10,17 @@ if __name__ == '__main__':
     w.geometry("500x500")
     w.title("나의 윈도우")
 
-    insert = Button(w, text="회원가입 처리", font=("궁서", 30), command = insert_infor)
-    update = Button(w, text="회원가입 수정", font=("궁서", 30), command = update_infor)
-    delete = Button(w, text="회원가입 삭제", font=("궁서", 30), command = delete_infor)
+    insert = Button(w, text="영화 입력", font=("궁서", 30), command = insert_infor)
+    update = Button(w, text="영화 수정", font=("궁서", 30), command = update_infor)
+    delete = Button(w, text="영화 삭제", font=("궁서", 30), command = delete_infor)
+    select = Button(w, text="영화 선택", font=("궁서", 30), command = select_infor)
+    select_all = Button(w, text="영화 모두선택", font=("궁서", 30), command = select_all_infor)
 
     insert.pack()
     update.pack()
     delete.pack()
+    select.pack()
+    select_all.pack()
 
     w.mainloop()
     
